e1d971929343b31e8f083bcf6194701e7dc718df
yaxyobekb <pubgn9642@gmail.com>
2025-03-25 22:46:30 +0500

    refactor bot message handling and user state management

--- index.js
+++ index.js
@@ -1,30 +1,105 @@
-// Bot configuration
-require("dotenv").config();
-const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.TOKEN, { polling: true });
-
-// Utils
-const { formatMessage } = require("./src/utils");
-
-// Bot listeners
-
-// /start command line
-bot.onText(/\/start/, (msg) => {
-  const message = msg.text;
-  const chatId = msg.chat.id;
-
-  console.log(msg);
-  
-});
+const {
+  create_user,
+  send_message,
+  check_command,
+  extract_numbers,
+  check_user_state,
+  get_user_language,
+  update_user_state,
+  update_user_language,
+  send_language_selection_message,
+} = require("./src/utils");
+const bot = require("./src/bot");
+const texts = require("./src/texts");
+const keyboards = require("./src/keyboards");
+const { users, devices } = require("./src/db");
 
 // Message listeners
-bot.on("message", async (msg) => {
-  const message = msg.text;
-  const chatId = msg.chat.id;
-});
+bot.on("message", async ({ from: user, text: message, chat }) => {
+  const chat_id = chat.id;
 
-// Callback query listeners
-bot.on("callback_query", async (query) => {
-  const queryData = query.data;
-  const chatId = query.message.chat.id;
+  // Start command
+  if (check_command("/start", message)) {
+    if (!users[chat_id]) {
+      return create_user(user);
+    }
+  }
+
+  // Update language
+  if (check_user_state(chat_id, "language_selection")) {
+    return update_user_language(chat_id, message);
+  }
+
+  // Get user from server
+  const user_storage = users[chat_id];
+  const user_state = user_storage?.state;
+  const user_language = user_storage?.language;
+
+  // Send language selection message
+  if (
+    (user_storage && !user_language) ||
+    check_command(texts.change_language, message)
+  ) {
+    return send_language_selection_message(chat_id);
+  }
+
+  // Help
+  if (check_command(texts.help, message)) {
+    return send_message(chat_id, texts.contact[get_user_language(chat_id)]);
+  }
+
+  // Back
+  if (check_command(texts.back, message) && user_state) {
+    const step = Number(extract_numbers(String(user_state)));
+
+    if (step === 0) {
+      send_message(chat_id, texts.cancel[get_user_language(chat_id)], {
+        reply_markup: {
+          resize_keyboard: true,
+          keyboard: keyboards.user.home(user_language),
+        },
+      });
+
+      return update_user_state(chat_id); // Clear user state
+    }
+
+    update_user_state(chat_id, `pricing_step_${step - 1}`);
+  }
+
+  console.log("😎: ", user_state);
+
+  // Pricing (Step 0)
+  if (
+    check_command(texts.pricing, message) ||
+    check_user_state(chat_id, "pricing_step_0")
+  ) {
+    send_message(chat_id, texts.nailing[user_language], {
+      reply_markup: {
+        resize_keyboard: true,
+        keyboard: keyboards.user.step_1(user_language),
+      },
+    });
+
+    return update_user_state(chat_id, "pricing_step_1");
+  }
+
+  // Step 1 (For pricing)
+  if (check_user_state(chat_id, "pricing_step_1")) {
+    const device = Object.keys(devices).find(
+      (device) => devices[device].name === message
+    );
+
+    if (!device) {
+      return send_message(chat_id, texts.invalid_device[user_language]);
+    }
+
+    send_message(chat_id, texts.nailing[user_language], {
+      reply_markup: {
+        resize_keyboard: true,
+        keyboard: keyboards.user.step_2(user_language, device),
+      },
+    });
+
+    // return update_user_state(chat_id, "pricing_step_2");
+  }
 });
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,143 @@
-const formatMessage = (title, description) => `*${title}*\n\n${description}`;
+const bot = require("./bot");
+const texts = require("./texts");
+const keyboards = require("./keyboards");
+const { users, languages } = require("./db");
 
-module.exports = { formatMessage };
+const extract_numbers = (text = "") => text?.replace(/\D/g, "");
+
+const format_message = (title, description) => `*${title}*\n\n${description}`;
+
+const send_message = (chatId, text, options) => {
+  bot.sendMessage(chatId, text, { ...options, parse_mode: "Markdown" });
+};
+
+const check_command = (command_data, command_text) => {
+  if (typeof command_data === "string" && command_data?.startsWith("/")) {
+    return command_data === command_text;
+  }
+
+  return Object.values(command_data).includes(command_text);
+};
+
+const check_user_state = (user_id, state) => {
+  if (!users[user_id]) {
+    console.error(
+      `Error: User with ID ${user_id} not found. Cannot check state.`
+    );
+    return false;
+  }
+
+  return users[user_id].state === state;
+};
+
+const get_user_language = (user_id) => {
+  return users[user_id]?.language;
+};
+
+const update_user_state = (user_id, state) => {
+  if (!users[user_id]) {
+    return console.error(
+      `Error: User with ID ${user_id} not found. Unable to update state.`
+    );
+  }
+
+  users[user_id].state = state;
+};
+
+const update_user_state_data = (user_id, key = "", data) => {
+  if (!users[user_id]) {
+    return console.error(
+      `Error: User with ID ${user_id} not found. Unable to update state data.`
+    );
+  }
+
+  const user_state_data = users[user_id].state_data;
+
+  if (user_state_data) user_state_data[key] = data;
+  else user_state_data = { [key]: data };
+};
+
+const update_user_language = (user_id, language) => {
+  const languages_name = Object.keys(languages).map(
+    (language) => languages[language].name
+  );
+
+  if (!languages_name.includes(language)) {
+    return send_message(
+      user_id,
+      format_message(
+        "Xatolik/Ошибка ❌",
+        "Til to'g'ri tanlanmadi! Quyidagi tugmalar orqali tilni qaytadan tanlab ko'ring.\n\nЯзык выбран неправильно! Попробуйте выбрать язык еще раз с помощью кнопок ниже. 👇"
+      )
+    );
+  }
+
+  const new_language = Object.keys(languages).find(
+    (language2) => languages[language2].name === language
+  );
+
+  send_message(
+    user_id,
+    format_message(
+      `${texts.success[new_language]} ✅`,
+      texts.language_selection_success(languages[new_language].name)[
+        new_language
+      ]
+    ),
+    {
+      reply_markup: {
+        resize_keyboard: true,
+        keyboard: keyboards.user.home(new_language),
+      },
+    }
+  );
+
+  update_user_state(user_id); // Clear user state
+  users[user_id].language = new_language; // Update user language
+};
+
+const send_language_selection_message = (user_id) => {
+  const formatted_languages = Object.keys(languages).map((lang) => {
+    return { text: languages[lang].name };
+  });
+
+  send_message(
+    user_id,
+    "Iltimos tilni tanlang!\n\nПожалуйста, выберите язык! 👇",
+    {
+      reply_markup: {
+        keyboard: [
+          [formatted_languages[0], formatted_languages[1]],
+          [formatted_languages[2]],
+        ],
+        resize_keyboard: true,
+      },
+    }
+  );
+
+  update_user_state(user_id, "language_selection");
+};
+
+const create_user = ({ first_name, username, language_code, id }) => {
+  const new_user = { id, username, first_name };
+
+  if (languages[language_code]) {
+    return (new_user.language = language_code);
+  }
+
+  users[id] = new_user;
+  send_language_selection_message(id);
+};
+
+module.exports = {
+  create_user,
+  send_message,
+  check_command,
+  format_message,
+  extract_numbers,
+  check_user_state,
+  update_user_state,
+  get_user_language,
+  update_user_language,
+  send_language_selection_message,
+};
