a1ffafec09da5421dc0e18be587594962c2a78a4
yaxyobekb <pubgn9642@gmail.com>
2025-04-03 13:35:25 +0500

    refactor pricing message handling and improve user state management

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,13 @@ const bot = require("./bot");
 const texts = require("./texts");
 const keyboards = require("./keyboards");
 const { users, languages, mandatory_channels } = require("./db");
+const use_calculate = require("./hooks/use_calculate");
 
 const format_message = (title, description) => `*${title}*\n\n${description}`;
 const extract_numbers = (text = "") => text?.match(/-?\d+/g)?.map(Number) || [];
 
-const send_message = (chatId, text, options) => {
-  bot.sendMessage(chatId, text, { ...options, parse_mode: "Markdown" });
+const send_message = (chat_id, text, options) => {
+  bot.sendMessage(chat_id, text, { ...options, parse_mode: "Markdown" });
 };
 
 const check_user_membership = async (user_id) => {
@@ -29,63 +30,8 @@ const send_membership_message = (chat_id, language) => {
   });
 };
 
-const check_command = (command_data, command_text) => {
-  if (typeof command_data === "string" && command_data?.startsWith("/")) {
-    return command_data === command_text;
-  }
-
-  return Object.values(command_data).includes(command_text);
-};
-
-const check_user_state = (user_id, state) => {
-  if (!users[user_id]) {
-    console.error(
-      `Error: User with ID ${user_id} not found. Cannot check state.`
-    );
-    return false;
-  }
-
-  return users[user_id].state === state;
-};
-
-const get_user_language = (user_id) => {
-  return users[user_id]?.language;
-};
-
-const update_user_state = (user_id, state) => {
-  if (!users[user_id]) {
-    return console.error(
-      `Error: User with ID ${user_id} not found. Unable to update state.`
-    );
-  }
-
-  users[user_id].state = state;
-};
-
-const update_user_state_data = (user_id, key = "", data) => {
-  const user = users[user_id];
-
-  if (!user) {
-    return console.error(
-      `Error: User with ID ${user_id} not found. Unable to update state data.`
-    );
-  }
-
-  if (!user.state_data) {
-    user.state_data = {};
-  }
-
-  user.state_data[key] = data;
-};
-
-const get_user_state_data = (user_id) => {
-  const user = users[user_id];
-
-  if (!user) {
-    return console.error(`Error: User with ID ${user_id} not found.`);
-  }
-
-  return user.state_data || {};
+const check_command = (command_1, command_2) => {
+  return command_1.trim().toLowerCase() === command_2.trim().toLowerCase();
 };
 
 const send_request_contact_message = (user_id, language) => {
@@ -96,7 +42,7 @@ const send_request_contact_message = (user_id, language) => {
     },
   });
 
-  update_user_state(user_id, "awaiting_contact");
+  update_state_name(user_id, "awaiting_contact");
 };
 
 const update_user_language = (user_id, language) => {
@@ -146,7 +92,7 @@ const update_user_language = (user_id, language) => {
     }
   );
 
-  update_user_state(user_id); // Clear user state
+  update_state_name(user_id); // Clear user state
 };
 
 const send_language_selection_message = (user_id) => {
@@ -161,7 +107,7 @@ const send_language_selection_message = (user_id) => {
     }
   );
 
-  update_user_state(user_id, "language_selection");
+  update_state_name(user_id, "language_selection");
 };
 
 const create_user = ({ first_name, username, language_code, id }) => {
@@ -177,79 +123,96 @@ const create_user = ({ first_name, username, language_code, id }) => {
   send_language_selection_message(id);
 };
 
-const send_pricing_message = (user) => {
-  const chat_id = user.id;
-  const language = user.language;
-  const state_data = user.state_data;
-  const model = state_data.model;
+const send_phone_pricing_message = ({ k, t, user, update_state_name }) => {
+  if (!user) return;
 
-  // Details
-  const price = model.price;
-  const model_name = model.name;
-  const damaged = state_data.damaged;
-  const color_name = state_data.color.name;
-  const color_minus = state_data.color.minus;
-  const box_and_doc = state_data.box_and_doc;
-  const storage_name = state_data.storage.name;
-  const country_name = state_data.country.name;
-  const country_minus = state_data.country.minus;
-  const storage_minus = state_data.storage.minus;
-  const battery_level_name = state_data.battery_level?.name;
-  const battery_level_minus = state_data.battery_level.minus;
-  const damaged_minus = damaged ? model.damaged[damaged] : 0;
+  const {
+    id: chat_id,
+    language_code,
+    state: {
+      data: {
+        cable,
+        box_docs,
+        device: {
+          deductions: {
+            countries,
+            accessories: { box: box_minus, cable: cable_minus },
+          },
+        },
+        model: { name: model_name },
+        country: { name: country_name },
+        memory: { name: memory_name, price: initial_price },
+        screen: { name: screen_name, percent: screen_percent },
+        appearance: { name: appearance_name, percent: appearance_percent },
+        battery_capacity: { name: battery_name, percent: battery_percent },
+      },
+    },
+  } = user;
 
-  // Calculate price
-  const box_and_doc_minus = box_and_doc ? model.box_and_doc : 0;
+  const { calculate_percentage } = use_calculate(initial_price);
+
+  // Pricing
+  const screen_price = calculate_percentage(screen_percent);
+  const battery_price = calculate_percentage(battery_percent);
+  const appearance_price = calculate_percentage(appearance_percent);
+  const cable_price = check_command(t("yes"), cable) ? 0 : cable_minus;
+  const box_docs_price = check_command(t("yes"), box_docs) ? 0 : box_minus;
+  const country_price = check_command(countries[0].name, country_name) ? 0 : 50;
+
+  // Calculate minus
   const minus =
-    color_minus +
-    damaged_minus +
-    storage_minus +
-    country_minus +
-    box_and_doc_minus +
-    battery_level_minus;
+    cable_price +
+    screen_price +
+    battery_price +
+    country_price +
+    box_docs_price +
+    appearance_price;
 
-  // Message text
+  const pricing_amount_message =
+    initial_price - minus > 0 ? `${initial_price - minus}$` : t("free");
+
+  // Message texts
   const message_text = `
 📱 *${model_name}
-🧠 ${storage_name}
-💥 ${damaged ? damaged : `${texts.excellent[language]} (0%)`} 
-🔋 ${battery_level_name}
+🧠 ${memory_name}
+✨ ${appearance_name}
+📺 ${screen_name}
+🔋 ${battery_name}
 🌎 ${country_name}
-🎨 ${color_name}
-📦 ${box_and_doc ? texts.exists[language] : texts.not_exists[language]}
-💰 ${price - minus}$*
+📦 ${box_docs}
+🔌 ${cable}
+💰 ${pricing_amount_message}*
 
-${texts.subscribe_prompt[language]}
+${t("subscribe_prompt")}
 
 [Telegram](https://t.me) | [Instagram](https://instagram.com)`;
 
   const share_text = `
 
 📱 **${model_name}
-🧠 ${storage_name}
-💥 ${damaged ? damaged : "0%"} 
-🔋 ${battery_level_name}
+🧠 ${memory_name}
+✨ ${appearance_name}
+📺 ${screen_name}
+🔋 ${battery_name}
 🌎 ${country_name}
-🎨 ${color_name}
-📦 ${box_and_doc ? texts.exists[language] : texts.not_exists[language]}
-💰 ${price - minus}$**`;
+📦 ${box_docs}
+🔌 ${cable}
+💰 ${pricing_amount_message}**`;
 
+  // Send message
   send_message(chat_id, message_text, {
     reply_markup: {
       resize_keyboard: true,
-      inline_keyboard: keyboards.user.share(language, share_text),
+      inline_keyboard: keyboards.user.share(language_code, share_text),
     },
     disable_web_page_preview: true,
   });
 
-  send_message(chat_id, texts.contact_admin[language], {
-    reply_markup: {
-      resize_keyboard: true,
-      keyboard: keyboards.user.home(language),
-    },
-  });
+  // Back to Home
+  send_message(chat_id, t("contact_admin"), k("home"));
 
-  user.state_data = {};
+  // Clear user state
+  update_state_name();
 };
 
 module.exports = {
@@ -258,15 +221,10 @@ module.exports = {
   check_command,
   format_message,
   extract_numbers,
-  check_user_state,
-  update_user_state,
-  get_user_language,
-  get_user_state_data,
   update_user_language,
-  send_pricing_message,
   check_user_membership,
-  update_user_state_data,
   send_membership_message,
+  send_phone_pricing_message,
   send_request_contact_message,
   send_language_selection_message,
 };
