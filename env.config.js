8f4ad25f18f2dbc3da1c7360399e996b569892c7
yaxyobekb <pubgn9642@gmail.com>
2025-04-03 13:29:51 +0500

    add environment configuration for bot token management

---
+++ env.config.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+const bot_token = process.env.TOKEN;
+
+module.exports = { bot_token };
--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
-// Bot configuration
-require("dotenv").config();
+const { bot_token } = require("../env.config");
 const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
+const bot = new TelegramBot(bot_token, { polling: true });
 
 module.exports = bot;
